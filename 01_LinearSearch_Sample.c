d55a2ac45162a5b4ee8f4edcf7b47d497948ec87
HumanITNagoya <51362998+HumanITNagoya@users.noreply.github.com>
2019-06-06 09:23:34 +0900

    Create 01_LinearSearch_Sample.c

---
+++ 01_LinearSearch_Sample.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+  /* 変数宣言 */
+  int i;
+  int tbl[10];
+  
+  /* ランダム関数の初期化 */
+  srand((unsigned)time(NULL));
+  
+  /* 0～99までのランダムな数値を配列に格納 */
+  
+  
+  return 0;
+}
